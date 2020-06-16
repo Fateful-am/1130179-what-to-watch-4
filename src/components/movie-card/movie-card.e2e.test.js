6671b904180e47d1d4b8cada17ec426009c2c921
Fateful-am <fateful.am@gmail.com>
2020-06-17 01:44:05 +0300

    Создаст e2e тест для MovieCard

---
+++ src/components/movie-card/movie-card.e2e.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Enzyme, {shallow} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import MovieCard from './movie-card';
+
+Enzyme.configure({
+  adapter: new Adapter(),
+});
+const movieCardId = 0;
+
+it(`Should movie card mouse entered`, () => {
+  const movieCardMouseEnter = jest.fn();
+
+  const movieCardScreen = shallow(
+      <MovieCard
+        onMouseEnter={movieCardMouseEnter}
+        id={movieCardId}
+        title={`Macbeth`}
+        onHeaderClick={() => {
+        }}
+      />
+  );
+
+  const movieCard = movieCardScreen.find(`.small-movie-card`);
+  const formSendPrevention = jest.fn();
+
+  movieCard.simulate(`mouseenter`, {
+    preventDefault: formSendPrevention,
+    currentTarget: {id: `mc-${movieCardId}`}
+  });
+
+  expect(movieCardMouseEnter).toHaveBeenCalledTimes(1);
+
+});
