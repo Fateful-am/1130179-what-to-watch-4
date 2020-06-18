819c50fd984876cdf31203bc317d348341c1a77a
Fateful-am <fateful.am@gmail.com>
2020-06-18 14:17:28 +0300

    Создаст компонент MoviePage - детальная информация о фильме

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export const MovieCard = ({id, title, imageSource, onTitleClick, onMouseEnter}) => {
+const MovieCard = ({id, title, imageSource, onTitleClick, onMouseEnter}) => {
   const handleMouseEnter = (evt) => {
     evt.preventDefault();
     onMouseEnter(id);
