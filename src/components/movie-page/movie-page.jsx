ad89374ff03f3d3f21cb54c7a185da5f0db9d064
Fateful-am <fateful.am@gmail.com>
2020-06-27 02:21:55 +0300

    Заменяет статическую разметку для страниц Overview, Details и Reviews на данные из моков

--- src/components/movie-page/movie-page.jsx
+++ src/components/movie-page/movie-page.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {MoviePageTabNames} from '../../consts.js';
+import {MoviePageTabNames, SHORT_ACTORS_LIST_COUNT, MOVIE_REVIEWS_COLUMN_COUNT} from '../../consts.js';
 import MoviePageTabs from '../movie-page-tabs/movie-page-tabs.jsx';
 
 class MoviePage extends React.Component {
@@ -22,6 +22,9 @@ class MoviePage extends React.Component {
 
   _getOverviewMarkup() {
     const {movie} = this.props;
+    const actors = movie.starring.split(`, `);
+    const shortActors = actors.length > SHORT_ACTORS_LIST_COUNT ? `${actors.slice(0, SHORT_ACTORS_LIST_COUNT).join(`, `)} and other` : movie.starring;
+
     return (
       <>
       <div className="movie-rating">
@@ -40,14 +43,23 @@ class MoviePage extends React.Component {
           })
         }
         <p className="movie-card__director"><strong>Director: {movie.director}</strong></p>
-        <p className="movie-card__starring"><strong>Starring: {movie.starring}</strong></p>
+        <p className="movie-card__starring"><strong>Starring: {shortActors}</strong></p>
       </div>
       </>
     );
   }
-
   _getDetailsMarkup() {
     const {movie} = this.props;
+
+    const actors = movie.starring.split(`, `).map((actor, i, array) => {
+      return (
+        <React.Fragment key={`actor-${i}-${movie.title}`}>
+          {i < array.length - 1 ? `${actor},` : actor}
+          {(() => i === array.length - 1 ? null : <br/>)()}
+        </React.Fragment>
+      );
+    });
+
     return (
       <div className="movie-card__text movie-card__row">
         <div className="movie-card__text-col">
@@ -58,18 +70,7 @@ class MoviePage extends React.Component {
           <p className="movie-card__details-item">
             <strong className="movie-card__details-name">Starring</strong>
             <span className="movie-card__details-value">
-                    Bill Murray, <br/>
-                    Edward Norton, <br/>
-                    Jude Law, <br/>
-                    Willem Dafoe, <br/>
-                    Saoirse Ronan, <br/>
-                    Tony Revoloru, <br/>
-                    Tilda Swinton, <br/>
-                    Tom Wilkinson, <br/>
-                    Owen Wilkinson, <br/>
-                    Adrien Brody, <br/>
-                    Ralph Fiennes, <br/>
-                    Jeff Goldblum
+              {actors}
             </span>
           </p>
         </div>
@@ -94,96 +95,55 @@ class MoviePage extends React.Component {
 
   _getReviewsMarkup() {
     const {movie} = this.props;
+
+    const renderReviews = (columnNum, reviews) => {
+      return (
+        reviews.map((review, i) => {
+          return (
+            <div
+              key={`review-${columnNum}-${i}-${movie.title}`}
+              className="review"
+            >
+              <blockquote className="review__quote">
+                <p className="review__text">{review.text}</p>
+
+                <footer className="review__details">
+                  <cite className="review__author">{review.author}</cite>
+                  <time className="review__date" dateTime="2016-12-24">{review.date}</time>
+                </footer>
+              </blockquote>
+
+              <div className="review__rating">{review.score}</div>
+            </div>
+          );
+        })
+      );
+    };
+
+    const renderColumns = (columns) => {
+      return (
+        columns.map((column, i) => {
+          return (
+            <div
+              key={`column-${i}-${movie.title}`}
+              className="movie-card__reviews-col"
+            >
+              {renderReviews(i, column)}
+            </div>
+          );
+        })
+      );
+    };
+
+    const columns = [];
+    const reviewsPerColumn = Math.ceil(movie.reviews.length / MOVIE_REVIEWS_COLUMN_COUNT);
+    for (let i = 0; i < MOVIE_REVIEWS_COLUMN_COUNT; i++) {
+      columns.push(movie.reviews.slice(i * reviewsPerColumn, (i + 1) * reviewsPerColumn));
+    }
+
     return (
       <div className="movie-card__reviews movie-card__row">
-        <div className="movie-card__reviews-col">
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">Discerning travellers and Wes Anderson fans will luxuriate in the glorious
-                Mittel-European kitsch of one of the directors funniest and most exquisitely designed movies in
-                years.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Kate Muir</cite>
-                <time className="review__date" dateTime="2016-12-24">December 24, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">8,9</div>
-          </div>
-
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">Andersons films are too precious for some, but for those of us willing to
-                lose ourselves in them, theyre a delight. The Grand Budapest Hotel is no different, except that he
-                has added a hint of gravitas to the mix, improving the recipe.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Bill Goodykoontz</cite>
-                <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">8,0</div>
-          </div>
-
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">I didnt find it amusing, and while I can appreciate the creativity, its an
-                hour and 40 minutes I wish I could take back.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Amanda Greever</cite>
-                <time className="review__date" dateTime="2015-11-18">November 18, 2015</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">8,0</div>
-          </div>
-        </div>
-        <div className="movie-card__reviews-col">
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">The mannered, madcap proceedings are often delightful, occasionally silly, and
-                here and there, gruesome and/or heartbreaking.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Matthew Lickona</cite>
-                <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">7,2</div>
-          </div>
-
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the
-                content is a little more adult.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Paula Fleri-Soler</cite>
-                <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">7,6</div>
-          </div>
-
-          <div className="review">
-            <blockquote className="review__quote">
-              <p className="review__text">It is certainly a magical and childlike way of storytelling, even if the
-                content is a little more adult.</p>
-
-              <footer className="review__details">
-                <cite className="review__author">Paula Fleri-Soler</cite>
-                <time className="review__date" dateTime="2016-12-20">December 20, 2016</time>
-              </footer>
-            </blockquote>
-
-            <div className="review__rating">7,0</div>
-          </div>
-        </div>
+        {renderColumns(columns)}
       </div>
     );
   }
--- src/consts.js
+++ src/consts.js
@@ -1,6 +1,8 @@
 export const START_MOVIE_COUNT = 8;
 export const PROMO_MOCK_INDEX = 8;
 export const PREVIEW_MOVIE_DELAY = 1000;
+export const SHORT_ACTORS_LIST_COUNT = 4;
+export const MOVIE_REVIEWS_COLUMN_COUNT = 2;
 
 export const PageKind = {
   MAIN: `main`,
