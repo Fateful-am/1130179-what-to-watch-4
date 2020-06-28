ec48d89c8ac58a376a61060869782f2dd01b4413
Fateful-am <fateful.am@gmail.com>
2020-06-28 23:06:12 +0300

    Добавит подключение Redux DevTools

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ import {mockMovies} from './mocks/films';
 import {PROMO_MOCK_INDEX} from './consts.js';
 import {reducer} from "./reducer.js";
 
-const store = createStore(reducer);
+const store = createStore(
+    reducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+);
 
 ReactDOM.render(
     <Provider store={store}>
