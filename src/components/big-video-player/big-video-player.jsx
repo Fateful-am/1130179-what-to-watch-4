3f48ab989108dec2151146bd35da0e2b3c038d1e
Fateful-am <fateful.am@gmail.com>
2020-07-12 17:21:54 +0300

    Добавляет иконку Play для видеоплейера

--- src/components/big-video-player/big-video-player.jsx
+++ src/components/big-video-player/big-video-player.jsx
@@ -4,8 +4,8 @@ import PropTypes from 'prop-types';
 const BigVideoPlayer = (props) => {
   const renderPlaySvg = () => {
     return (
-      <svg viewBox="0 0 19 19" width="19" height="19">
-        <use xlinkHref="#play-s"/>
+      <svg width="19" height="19" viewBox="0 0 19 19" fill="none">
+        <path fillRule="evenodd" clipRule="evenodd" d="M0 0L19 9.5L0 19V0Z" fill="#EEE5B5"/>
       </svg>
     );
   };
